6ee2efab70b85368933931cfa09b9bc662a216a3
Syzygia <bigmegaking@mail.com>
2020-08-04 22:06:49 +1000

    add test compatibility json/csv wise

--- database.py
+++ database.py
@@ -7,6 +7,9 @@ client = MongoClient(
     "mongodb+srv://AdminFinBot:vmwbMwH7c5nrWZTE@cluster0.pyff5.mongodb.net/finbot_db?retryWrites=true&w=majority")
 db = client.finbot_db
 
+ID, PARENT_ID, TEXT, TYPE, VALUE = range(5)
+CONVERSATION_END = 0.1
+
 
 def update_dialog(_id, path):
     with open(path, 'r', encoding='utf-8') as json_data:
@@ -30,32 +33,50 @@ def insert_dialog(path):
     dialog = {'dialog': json_string}
     db.dialogs.insert_one(dialog)
 
-
 def convert_csv_to_json(path):
     json_data = dict()
     json_data['replies'] = list()
     json_data['lines'] = list()
+    json_data['custom_choices'] = list()
 
     csv_lines = open(path, 'r', encoding='utf-8').readlines()
     questions = dict()
     answers = collections.OrderedDict()
-    # csv_lines[0] = csv_lines[0].encode('utf-8-sig').decode('utf-8-sig')
-    # csv_lines = csv_lines[0].split('"')
+    csv_lines[0] = csv_lines[0].encode('utf-8-sig').decode('utf-8-sig')
     for line in csv_lines:
-        line = line.split('"')[1].split('~')
+        line = line.rstrip()
+        line = line.split('~')
+        del (line[0])
         # line 0-id 1-parent id 2-text
 
-        if (not line[1] in questions):
-            questions[line[0]] = dict()
-            questions[line[0]]['text'] = line[2]
-            questions[line[0]]['replies'] = dict()
-            if (line[1] in answers):
-                questions[answers[line[1]]]['replies'][list(answers.keys()).index(line[1])] = len(questions) - 1
+        if (not line[PARENT_ID] in questions):
+            questions[line[ID]] = dict()
+            questions[line[ID]]['text'] = line[TEXT]
+            questions[line[ID]]['replies'] = dict()
+            if (line[PARENT_ID] in answers):
+                questions[answers[line[PARENT_ID]]]['replies'][list(answers.keys()).index(line[PARENT_ID])] = \
+                    len(questions) - 1
 
         else:
-            answers[str(line[0])] = line[1]
+            if (len(line) >= 4 and line[TYPE] == 'test_question'):
+                questions[line[ID]] = dict()
+                questions[line[ID]]['text'] = line[TEXT]
+                questions[line[ID]]['replies'] = dict()
+                questions[line[ID]]['type'] = line[TYPE]
+                if (line[PARENT_ID] in questions):
+                    for k, v in questions[line[PARENT_ID]]['replies'].items():
+                        v['leads'] = len(questions) - 1
+
+            answers[str(line[ID])] = line[PARENT_ID]
             json_data['replies'].append(line[2])
-            questions[line[1]]['replies'][(len(json_data['replies']) - 1)] = 0.1
+            if (len(line) >= 4 and line[TYPE] == 'test_answer'):
+                questions[line[PARENT_ID]]['replies'][(len(json_data['replies']) - 1)] = {'value': line[VALUE],
+                                                                                          'leads': CONVERSATION_END}
+            else:
+                questions[line[PARENT_ID]]['replies'][(len(json_data['replies']) - 1)] = CONVERSATION_END
+
+            if (len(line) >= 4 and line[TYPE] == 'custom_choice'):
+                json_data['custom_choices'].append(len(json_data['replies']) - 1)
 
     for key, val in questions.items():
         json_data['lines'].append(val)
@@ -114,5 +135,5 @@ def reset_user_dialog_status(user_id, dialog_id):
 
                          })
 
-# convert_csv_to_json('Untitled_6.csv')
+# convert_csv_to_json('FINBOT_AI_1 (1).csv')
 # insert_dialog('data.json')
